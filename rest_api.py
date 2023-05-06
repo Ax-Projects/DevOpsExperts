70b8d8ab4ae3cf4fcaf42d48f6e22f3ecf97e62b
Ax-Projects <theamsalem@gmail.com>
2023-05-06 12:05:03 +0300

    added error catching for every method

--- rest_api.py
+++ rest_api.py
@@ -22,40 +22,47 @@ def user(user_id):
                 return {"status": "error", "reason": "no such id"}, 500
         # Catching KeyError type error and printing to terminal
         except Exception as e:
-            print(f"Error in GET method: {e}")
+            print(f"Error in GET method:\n {e}")
 
     elif request.method == "POST":
-        # getting the json data payload from request
-        request_data = request.json
-        # Checking if user_id exists in the DB
-        userData = db.get_user_data(user_id)
-        if userData != None:
-            return {"status": "error", "reason": "ID already exists"}, 500
-        else:
-            # treating request_data as a dictionary to get a specific value from key
-            user_nm = request_data.get("user_name")
-            status = db.create_user(user_id=user_id, user_name=user_nm)
-            if status == True:
-                return {"status": "OK", "user_added": user_nm}, 200
+        # Error catching for POST method
+        try:
+            # getting the json data payload from request
+            request_data = request.json
+            # Checking if user_id exists in the DB
+            userData = db.get_user_data(user_id)
+            if userData != None:
+                return {"status": "error", "reason": "ID already exists"}, 500
             else:
-                return {f"error": status}, 400
+                # treating request_data as a dictionary to get a specific value from key
+                user_nm = request_data.get("user_name")
+                status = db.create_user(user_id=user_id, user_name=user_nm)
+                if status == True:
+                    return {"status": "OK", "user_added": user_nm}, 200
+                else:
+                    return {f"error": status}, 400
+        except Exception as e:
+            print(f"Error in  POST method:\n {e}")
 
     elif request.method == "PUT":
-        # getting the json data payload from request
-        request_data = request.json
-        # Checking if user_id exists in the DB
-        userData = db.get_user_data(user_id)
-        if userData == None:
-            return {"status": "error", "reason": "No such ID"}, 500
-        elif userData != None:
-            newUserName = request_data.get("user_name")
-            status = db.update_user(user_id=userData, user_name=newUserName)
-            if status == True:
-                return {"status": "OK", "user_updated": userData}, 200
+        try:
+            # getting the json data payload from request
+            request_data = request.json
+            # Checking if user_id exists in the DB
+            userData = db.get_user_data(user_id)
+            if userData == None:
+                return {"status": "error", "reason": "No such ID"}, 500
+            elif userData != None:
+                newUserName = request_data.get("user_name")
+                status = db.update_user(user_id=userData, user_name=newUserName)
+                if status == True:
+                    return {"status": "OK", "user_updated": userData}, 200
+                else:
+                    return {f"sql error": status}, 400
             else:
-                return {f"sql error": status}, 400
-        else:
-            return {"status": "error", "reason": status}, 400
+                return {"status": "error", "reason": status}, 400
+        except Exception as e:
+            print(f"Error in  PUT method:\n {e}")
 
     elif request.method == "DELETE":
         # Error catching in DELETE request for user name by user_id
@@ -70,7 +77,7 @@ def user(user_id):
                 else:
                     return {"sql error": status}, 400
         except Exception as e:
-            print(f"Error in DELTE method: {e}")
+            print(f"Error in DELETE method:\n {e}")
 
 
 app.run(host="127.0.0.1", debug=True, port=5000)
