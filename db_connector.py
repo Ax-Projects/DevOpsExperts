ec89232218108a7751b8bca5cf5b46b4e5eb11e6
Ax-Projects <theamsalem@gmail.com>
2023-05-06 11:38:02 +0300

    db_connector working, rest_api half done

--- db_connector.py
+++ db_connector.py
@@ -1,75 +1,183 @@
 import pymysql
-
-conn = pymysql.connect(
-    host="sql7.freemysqlhosting.net",
-    user="sql7615057",
-    password="nUYs7WA6Mx",
-    db="sql7615057",
-    port=3306,
-)
-conn.autocommit(True)
-cursor = conn.cursor()
+import datetime
 
 schema_name = "sql7615057"
 
 
-def check_Table():
-    cursor.execute(
+def check_table_exists():
+    try:
+        conn = pymysql.connect(
+            host="sql7.freemysqlhosting.net",
+            user="sql7615057",
+            password="nUYs7WA6Mx",
+            db="sql7615057",
+            port=3306,
+        )
+        conn.autocommit(True)
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+        SELECT COUNT(*)
+        FROM information_schema.tables
+        WHERE table_name = 'users'
         """
-    SELECT * FROM INFORMATION_SCHEMA.TABLES
-    WHERE TABLE_NAME = 'users'
-    """
+        )
+        if cursor.fetchone()[0] == 1:
+            cursor.close()
+            conn.close()
+            return True
+        else:
+            cursor.close()
+            conn.close()
+            return False
+    except pymysql.Error as e:
+        conn.close()
+        return e
+
+
+# Inserting data into table
+def create_table():
+    try:
+        conn = pymysql.connect(
+            host="sql7.freemysqlhosting.net",
+            user="sql7615057",
+            password="nUYs7WA6Mx",
+            db="sql7615057",
+            port=3306,
+        )
+        conn.autocommit(True)
+        cursor = conn.cursor()
+        statementToExecute = f"CREATE TABLE `{schema_name}`.`users`(`user_id` INT NOT NULL,`user_name` VARCHAR(50) NOT NULL,`creation_date` VARCHAR(50) NOT NULL, PRIMARY KEY (`id`));"
+        cursor.execute(statementToExecute)
+        cursor.close()
+        conn.close()
+        return True
+    except pymysql.Error as e:
+        # print(e)
+        return e
+
+
+# Getting users table from the Database
+def get_users():
+    conn = pymysql.connect(
+        host="sql7.freemysqlhosting.net",
+        user="sql7615057",
+        password="nUYs7WA6Mx",
+        db="sql7615057",
+        port=3306,
     )
-    if cursor.fetchone() is not None:
-        return 1
-    else:
-        return 0
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM {schema_name}.users;")
+    output = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return output
 
 
-# f"IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '{schema_name}' AND  TABLE_NAME = 'users'))"
-
-print(check_Table())
-# cursor.execute(
-#     f"IF OBJECT_ID (N'users', N'U') IS NOT NULL SELECT 1 AS res ELSE SELECT 0 AS res;"
-# )
-# Creating users table in the Database
-# if check_Table() == False:
-#     try:
-#         statementToExecute = f"CREATE TABLE `{schema_name}`.`users`(`user_id` INT NOT NULL,`user_name` VARCHAR(50) NOT NULL, `creation_date` VARCHAR(50) NOT NULL, PRIMARY KEY (`user_id`));"
-#         cursor.execute(statementToExecute)
-#     except Exception as e:
-#         print(e)
-
-
-# def get_users():
-#     cursor.execute(f"SELECT * FROM {schema_name}.users;")
-#     for row in cursor:
-#         print(row)
-
-
-# # Inserting data into table
-# cursor.execute(f"INSERT into {schema_name}.users (name, id) VALUES ('Orr', 30)")
-# get_users()
-
-# # Instering More users
-# cursor.execute(f"INSERT into {schema_name}.users (name, id) VALUES ('Tal', 2)")
-# cursor.execute(f"INSERT into {schema_name}.users (name, id) VALUES ('Roey', 5)")
-
-# # Getting users from the table
-# cursor.execute(f"SELECT * FROM {schema_name}.users;")
-# for row in cursor:
-#     print(row)
-
-# # Updating data in the table
-# cursor.execute(f"UPDATE {schema_name}.users SET id = '10' WHERE name = 'Orr'")
-# cursor.execute(f"UPDATE {schema_name}.users SET name = 'Roey-Shmu' WHERE id = '5'")
-
-# # Deleting the data from the table
-# cursor.execute(f"DELETE FROM {schema_name}.users WHERE name = 'Orr'")
-# get_users()
+def get_user_data(user_id: str):
+    try:
+        conn = pymysql.connect(
+            host="sql7.freemysqlhosting.net",
+            user="sql7615057",
+            password="nUYs7WA6Mx",
+            db="sql7615057",
+            port=3306,
+        )
+        conn.autocommit(True)
+        cursor = conn.cursor()
+        cursor.execute(
+            f"SELECT user_name FROM {schema_name}.users WHERE user_id = {user_id};"
+        )
+        output = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        return output[0]
+    except pymysql.Error as e:
+        return e
+    except TypeError as e:
+        return None
 
 
-cursor.execute(f"DROP TABLE `{schema_name}`.`users`;")
+# Inserting data into table
+def create_user(user_id: str, user_name: str):
+    timenow = str(datetime.datetime.now()).split(".")[0]
+    try:
+        conn = pymysql.connect(
+            host="sql7.freemysqlhosting.net",
+            user="sql7615057",
+            password="nUYs7WA6Mx",
+            db="sql7615057",
+            port=3306,
+        )
+        conn.autocommit(True)
+        cursor = conn.cursor()
+        cursor.execute(
+            f"INSERT into {schema_name}.users (user_name, user_id, creation_date) VALUES ('{user_name}', '{user_id}', '{timenow}');"
+        )
+        cursor.close()
+        conn.close()
+        return True
+    except pymysql.Error as e:
+        return e
 
-cursor.close()
-conn.close()
+
+# Updating data in the table
+def update_user(user_id: str, user_name: str):
+    try:
+        conn = pymysql.connect(
+            host="sql7.freemysqlhosting.net",
+            user="sql7615057",
+            password="nUYs7WA6Mx",
+            db="sql7615057",
+            port=3306,
+        )
+        conn.autocommit(True)
+        cursor = conn.cursor()
+        cursor.execute(
+            f"UPDATE {schema_name}.users SET user_name = '{user_name}' WHERE user_id = '{user_id}'"
+        )
+        cursor.close()
+        conn.close()
+        return True
+    except pymysql.Error as e:
+        return e
+
+
+# Deleting the data from the table
+def delete_user(user_id: str):
+    try:
+        conn = pymysql.connect(
+            host="sql7.freemysqlhosting.net",
+            user="sql7615057",
+            password="nUYs7WA6Mx",
+            db="sql7615057",
+            port=3306,
+        )
+        conn.autocommit(True)
+        cursor = conn.cursor()
+        cursor.execute(f"DELETE FROM {schema_name}.users WHERE user_id = '{user_id}'")
+        cursor.close()
+        conn.close()
+        return True
+    except pymysql.Error as e:
+        return e
+
+
+# Function for dropping the table
+def drop_table():
+    try:
+        conn = pymysql.connect(
+            host="sql7.freemysqlhosting.net",
+            user="sql7615057",
+            password="nUYs7WA6Mx",
+            db="sql7615057",
+            port=3306,
+        )
+        conn.autocommit(True)
+        cursor = conn.cursor()
+        cursor.execute(f"DROP TABLE `{schema_name}`.`users`;")
+        cursor.close()
+        conn.close()
+        return True
+    except pymysql.Error as e:
+        return e
--- rest_api.py
+++ rest_api.py
@@ -1,46 +1,78 @@
-import requests
-
-operation = True
-
-
-def test():
-    task = input(
-        "This is a test sscript for REST requests.\nPlease enter a REST method you want to test (get, post, put, delete)\nTo exit the script write 'exit'\n"
-    ).lower()
-    return task
+# import requests
+from flask import Flask, request
+import db_connector as db
 
 
-task = test()
-while operation == True:
-    if task == "get":
-        res = requests.get("http://127.0.0.1:5000/users/1")
-        if res.ok:
-            print(res.json())
+# res = requests.post("http://127.0.0.1:5000/users/1", json={"user_name": "Orr Amsalem"})
+# if res.ok:
+#     print(res.json())
+
+
+app = Flask(__name__)
+
+# local users storage
+users: dict = {}
+
+
+@app.route("/", methods=["GET"])
+def root():
+    return "server is online", 200
+
+
+@app.route("/users/<user_id>", methods=["GET", "POST", "DELETE", "PUT"])
+def user(user_id):
+    if request.method == "GET":
+        # Error catching in GET request for user name by user_id
+        try:
+            userData = db.get_user_data(user_id)
+            # Checking if user_id exists in users dictionary
+            if userData != None:
+                return {"status": "OK", "user_name": userData}, 200
+            elif userData == None:
+                return {"status": "error", "reason": "no such id"}, 500
+        # Catching KeyError type error and printing to terminal
+        except Exception as e:
+            print(f"Error in GET method: {e}")
+
+    elif request.method == "POST":
+        # getting the json data payload from request
+        request_data = request.json
+        # Checking if user_id exists in users dictionary
+        userData = db.get_user_data(user_id)
+        if userData != None:
+            return {"status": "error", "reason": "ID already exists"}, 500
         else:
-            task = test()
-            break
-    elif task == "post":
-        res = requests.post(
-            "http://127.0.0.1:5000/users/1", json={"user_name": "Orr Amsalem"}
-        )
-        if res.ok:
-            print(res.json())
+            # treating request_data as a dictionary to get a specific value from key
+            user_nm = request_data.get("user_name")
+            status = db.create_user(user_id=user_id, user_name=user_nm)
+            if status == True:
+                return {"status": "OK", "user_added": user_nm}, 200
+            else:
+                return {f"error": status}, 400
+
+    elif request.method == "PUT":
+        # getting the json data payload from request
+        request_data = request.json
+        # Checking if user_id exists in users dictionary
+        if users.get(user_id):
+            users[user_id] = request_data.get("user_name")
+            return {"status": "OK", "user_updated": users[user_id]}, 200
         else:
-            task = test()
-            break
-    elif task == "put":
-        res = requests.put(
-            "http://127.0.0.1:5000/users/1", json={"user_name": "John Smith"}
-        )
-        if res.ok:
-            print(res.json())
-    elif task == "delete":
-        res = requests.delete("http://127.0.0.1:5000/users/1")
-        if res.ok:
-            print(res.json())
-    elif task == "exit":
-        operation = False
-    else:
-        print(
-            "You must provide one of the following input parameters: get, post, put or delete"
-        )
+            # treating request_data as a dictionary to get a specific value from key
+            return {"status": "error", "reason": "No such ID"}, 500
+
+    elif request.method == "DELETE":
+        # Error catching in DELETE request for user name by user_id
+        try:
+            # Checking if user_id exists in users dictionary
+            if users.get(user_id):
+                users.pop(user_id)
+                return {"status": "OK", "user_deleted": user_id}, 200
+            else:
+                return {"status": "error", "reason": "no such id"}, 500
+        # Catching KeyError type error and printing to terminal
+        except Exception as e:
+            print(f"Error in DELTE method: {e}")
+
+
+app.run(host="127.0.0.1", debug=True, port=5000)
