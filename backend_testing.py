dc913b60d4f54899292f24dee29843865e83dc65
Ax-Projects <theamsalem@gmail.com>
2023-06-24 14:56:28 +0300

    removed frontend testing froim jenkinsfile

--- backend_testing.py
+++ backend_testing.py
@@ -1,19 +1,11 @@
 # from unittest import TestCase
 import requests
-import pymysql
 
 # Setting some variables for easy testing
 userName = "backend test user"
 userID = "5"
 url = f"http://localhost:5000/users/{userID}"
 jsonBody = {"user_name": f"{userName}"}
-
-# Database connection settings
-_host = "devopsexperts-devdb.cocqzivxlu19.eu-north-1.rds.amazonaws.com"
-_user = "devopsroot"
-_password = "r83nHqs7wew9Gycr"
-_db = "devopsdb"
-_port = 3306
 
 # POST request test
 post = requests.post(url=url, json=jsonBody)
@@ -38,27 +30,3 @@ elif get.status_code == 500:
     print("User validation failed. ID does not exist in the database")
 else:
     print("GET request failed")
-
-# Check if the user exists in the database directly
-try:
-    conn = pymysql.connect(
-        host=_host,
-        user=_user,
-        password=_password,
-        db=_db,
-        port=_port,
-    )
-    cursor = conn.cursor()
-    cursor.execute(f"SELECT user_name FROM {_db}.users WHERE user_id = {userID};")
-    output = cursor.fetchone()
-    cursor.close()
-    conn.close()
-    print(output)
-    ### Pretty print
-    # print(output[0])
-except pymysql.Error as e:
-    print(e)
-
-# Comparing the results
-if output[0] == userName:
-    print("User retrieved successfully from database")
